eb38826014969d49d20551feee4974a6759f3eb2
tsunekyos <tsunekyos@gmail.com>
2024-09-12 19:57:53 +0900

    ogデフォルト追加して、lambdaの記載追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'aws-lambda',
+    serveStatic: true,
+  },
+  app: {
+    head: {
+      meta: [
+        { property: 'og:image', content: 'https://example.com/og-default.png' },
+        { property: 'og:title', content: 'あなたのサービス名' },
+        { property: 'og:description', content: 'あなたのサービスのデフォルトの説明文' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://example.com' }
+      ]
+    }
+  }
 })
